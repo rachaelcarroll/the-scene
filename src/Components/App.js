0c31c77a75befd78112531d240b6fd15d70b37fd
rachaelcarroll <rachaelcarroll.m@gmail.com>
2021-08-05 13:57:31 -0400

    Update return in App with Routes for component rendering

--- src/Components/App.js
+++ src/Components/App.js
@@ -4,31 +4,24 @@ import { Header } from './Header.js';
 import { Quote } from './Question';
 import { Faves } from './QuestionBoard';
 import { Welcome } from './QuestionDetails';
-import { fetchQuote } from '../Utilities/apiCalls';
+// import { fetchQuote } from '../Utilities/apiCalls';
 import PropTypes from 'prop-types';
 
 export const App = () => {
-  const [ loading, setLoading ] = useState(true);
-  const [ error, setError ] = useState('');
-  
+  // const [ loading, setLoading ] = useState(true);
+  // const [ error, setError ] = useState('');
   
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header />
+      <main>
+        <Switch>
+          <Route exact path= '/welcome' component={ Welcome }/>
+          <Route exact path= '/quote' component={ Quote }/>
+          <Route exact path= '/faves' component={ Faves }/>
+        </Switch>
+      </main>
+    </>
   );
 }
 
