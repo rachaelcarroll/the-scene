4559136b6cbf585db911c4f726616e72f6886d92
rachaelcarroll <rachaelcarroll.m@gmail.com>
2021-08-05 13:38:27 -0400

    create fetch call for random quote

---
+++ src/Utilities/apiCalls.js
@@ -0,0 +1,9 @@
+export const fetchQuote = () => {
+    return fetch('https://binaryjazz.us/wp-json/genrenator/v1/story/1')
+    .then(response => {
+      if (!response.ok) {
+        throw Error()
+      }
+      return response.json()
+    })
+  }
