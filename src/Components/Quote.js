2aeb2e83002a451535af6524b96e4c102bf01093
rachaelcarroll <rachaelcarroll.m@gmail.com>
2021-08-05 19:54:12 -0400

    Fix bug in shuffling quote

--- src/Components/Quote.js
+++ src/Components/Quote.js
@@ -2,40 +2,52 @@ import React, { useState, useEffect } from 'react';
 import { NavLink } from 'react-router-dom';
 // import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import PropTypes from 'prop-types';
-import { fetchQuote } from '../Utilities/apiCalls';
+// import { fetchQuote } from '../Utilities/apiCalls';
 
 
-export const Quote = ({faves}) => {
+export const Quote = ({faves, saveFave}) => {
     const [ quote, setQuote ] = useState('');
-    // const [ faves, setFaves ] = useState([]);
+    const [ isLoading, setIsLoading ] = useState(false)
+    const [ error, setError ] = useState('');
 
-    const getQuote = () => {
-        fetchQuote()
-            .then(quote => setQuote(quote))
+    const getQuote = async () => {
+        setIsLoading(true)
+        const url = 'https://binaryjazz.us/wp-json/genrenator/v1/story/1'
+        try {
+            let response = await fetch(url)
+            const quote = await response.json()
+            setQuote(quote)
+            setIsLoading(false)
+            console.log("FETCHED QUOTE", quote)
+        } catch(error) {
+            setError(error.message)
+        }
     }
 
+    
     useEffect(() => {
         getQuote()
     }, [])
 
     const handleClick = (e) => {
         e.preventDefault()
+        setQuote('');
         getQuote();
     }
 
-    const saveFave = (e) => {
-        e.preventDefault()
-    //prepends the favorite to the front of the list
-        setFaves([quote].concat(faves))
-        console.log(faves)
-    }
+    // const saveFave = (e) => {
+    //     e.preventDefault()
+    // //prepends the favorite to the front of the list
+    //     setFaves([quote].concat(faves))
+    //     console.log(faves)
+    // }
 
     return (
         <section className='quote-container'>
             <article>{quote}</article>
             <div className='button-options'>
                 <button className='shuffle-quote' onClick={(e) => handleClick(e)}>Shuffle that scene.</button>
-                <button className='save-to-faves' onClick={(e) => saveFave(e)}>Save to faves.</button>
+                <button className='save-to-faves' onClick={() => saveFave(quote)}>Save to faves.</button>
             </div>
         </section>
     )
