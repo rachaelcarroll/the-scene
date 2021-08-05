95abdbcabf82721e126a60a10fe3d77474d8a025
rachaelcarroll <rachaelcarroll.m@gmail.com>
2021-08-05 18:19:54 -0400

    Update quotes to quote to render button

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -10,16 +10,16 @@ export const Header = () => {
                 <h1>TS</h1>
             </header>
             <div className='nav-btns'>
-            <Route exact path='/quotes' render={() => {
+            <Route exact path='/quote' render={() => 
                 <NavLink to ='/faves'>
-                <button className='fave-btn'>Faves</button>
+                    <button className='fave-btn'>Faves</button>
                 </NavLink>
-            }} />
-            <Route exact path='/faves' render={() => {
+            } />
+            <Route exact path='/faves' render={() => 
                 <NavLink to='/quote'>
                     <button className='return-to-quote'>Back To Home</button>
                 </NavLink>
-            }} />
+            } />
             </div>
         </nav>
     )
