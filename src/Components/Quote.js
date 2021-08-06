e43841fd4855fad545d63f53717dcfc1c7d28e3b
rachaelcarroll <rachaelcarroll.m@gmail.com>
2021-08-06 14:39:02 -0400

    Remove commented out code in Quote

--- src/Components/Quote.js
+++ src/Components/Quote.js
@@ -1,8 +1,6 @@
-import React, { useState, useEffect } from 'react';
-import { NavLink } from 'react-router-dom';
+import React from 'react';
 // import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import PropTypes from 'prop-types';
-// import { fetchQuote } from '../Utilities/apiCalls';
 
 
 export const Quote = ({ saveFave, quote, getQuote }) => {
