e3628bd7e03acbd335fcc35e3af276ea42346993
rachaelcarroll <rachaelcarroll.m@gmail.com>
2021-08-06 14:20:06 -0400

    Import Fave component

--- src/Components/FavesContainer.js
+++ src/Components/FavesContainer.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Fave } from './Fave';
 import { NavLink } from 'react-router-dom';
 // import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import PropTypes from 'prop-types';
