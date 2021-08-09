f45698b026a4eb2f401a951a15a66544034ce217
rachaelcarroll <rachaelcarroll.m@gmail.com>
2021-08-09 11:36:16 -0400

    Add onclick for add fave

--- src/Components/Quote.js
+++ src/Components/Quote.js
@@ -3,18 +3,24 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 
-export const Quote = ({ saveFave, quote, getQuote }) => {
+export const Quote = ({ saveFave, quote, getQuote, bounceHeart }) => {
 
     const passFave = (quote) => {
         console.log("QUOTE!!", quote)
         saveFave(quote)
+    }
+
+    const onClick = (event) => {
+        event.preventDefault();
+        passFave(quote);
+        bounceHeart()
     }
     return (
         <section className='quote-container'>
             <article className='quote'>"{quote}"</article>
             <div className='button-options'>
                 <button className='shuffle-quote' onClick={() => getQuote()}>shuffle that scene.</button>
-                <button className='save-to-faves' onClick={() => passFave(quote)}>wow, that's good. save to faves.</button>
+                <button className='save-to-faves' onClick={(event) => onClick(event)}>wow, that's good. save to faves.</button>
             </div>
         </section>
     )
