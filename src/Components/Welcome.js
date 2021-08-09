966a9abf3ec7fd7e012e89095f6072cf4aa35558
rachaelcarroll <rachaelcarroll.m@gmail.com>
2021-08-09 18:24:27 -0400

    Update italics for header in welcome message

--- src/Components/Welcome.js
+++ src/Components/Welcome.js
@@ -6,7 +6,7 @@ export const Welcome = () => {
     return (
         <>
         <article className='welcome-container'>
-            <h1 className='welcome'>Welcome to <i>the scene...</i></h1>
+            <h1 className='welcome'>Welcome to the<i>scene</i>...</h1>
             <p className='welcome-intro'>We take the effort out of sounding <i>hip</i> in the music scene.</p>
             <p className='welcome-question'>Ready to spark up a convo?</p>
             <NavLink to= '/quote'>
