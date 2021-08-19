65fa9a46dc7e4fbbabb530c2cad60f99122a52d7
rachaelcarroll <rachaelcarroll.m@gmail.com>
2021-08-19 17:37:36 -0400

    Comment out first test in cypress main page

--- cypress/integration/main_page.js
+++ cypress/integration/main_page.js
@@ -4,9 +4,9 @@ describe("Main Page", () => {
         cy.initialVisit();
       })
 
-    it('Should be able to visit the site and see a welcome message displayed', () => {
-        cy.contains('Welcome to the scene...')
-    })
+    // it('Should be able to visit the site and see a welcome message displayed', () => {
+    //     cy.contains('Welcome to the scene...')
+    // })
 
     it('Should display a button to click', () => {
         cy.get('button').contains('click here')
